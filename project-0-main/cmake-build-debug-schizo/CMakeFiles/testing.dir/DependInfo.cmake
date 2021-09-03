3538a6c3c1f706201140c062da235503af2fcaeb
dallinfunk <dallinfunk23@gmail.com>
2021-09-03 17:18:59 -0600

    Page count error

---
+++ project-0-main/cmake-build-debug-schizo/CMakeFiles/testing.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/d/dallinbf/cs236/proj0/Book.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/Book.cpp.o"
+  "/users/guest/d/dallinbf/cs236/proj0/BookCreator.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/BookCreator.cpp.o"
+  "/users/guest/d/dallinbf/cs236/proj0/BookListCreator.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/BookListCreator.cpp.o"
+  "/users/guest/d/dallinbf/cs236/proj0/Info.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/Info.cpp.o"
+  "/users/guest/d/dallinbf/cs236/proj0/Library.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/Library.cpp.o"
+  "/users/guest/d/dallinbf/cs236/proj0/main.cpp" "/users/guest/d/dallinbf/cs236/proj0/cmake-build-debug-schizo/CMakeFiles/testing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project-0-main/cmake-build-debug-schizo/CMakeFiles/testing.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testing.dir/Book.cpp.o"
+  "CMakeFiles/testing.dir/BookCreator.cpp.o"
+  "CMakeFiles/testing.dir/BookListCreator.cpp.o"
+  "CMakeFiles/testing.dir/Info.cpp.o"
+  "CMakeFiles/testing.dir/Library.cpp.o"
+  "CMakeFiles/testing.dir/main.cpp.o"
+  "testing"
+  "testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
